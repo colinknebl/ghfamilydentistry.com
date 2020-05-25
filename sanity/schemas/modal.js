4504900e873123d6c4b5367864fb5431bfd80134
Colin Knebl <colinknebl@outlook.com>
2020-05-25 07:35:01 -0400

    update sanity schema

--- sanity/schemas/modal.js
+++ sanity/schemas/modal.js
@@ -8,11 +8,11 @@ export default {
             title: 'Title',
             type: 'string',
         },
-        {
-            name: 'image',
-            title: 'Image',
-            type: 'image',
-        },
+        // {
+        //     name: 'image',
+        //     title: 'Image',
+        //     type: 'image',
+        // },
         {
             name: 'publishedAt',
             title: 'Published at',
@@ -21,50 +21,14 @@ export default {
         {
             name: 'content',
             title: 'Content',
-            type: 'array',
-            of: [
-                {
-                    type: 'block',
-                    styles: [
-                        {
-                            title: 'Heading 1',
-                            value: 'h1',
-                        },
-                        {
-                            title: 'Heading 2',
-                            value: 'h2',
-                        },
-                        {
-                            title: 'Heading 3',
-                            value: 'h3',
-                        },
-                        {
-                            title: 'Heading 4',
-                            value: 'h4',
-                        },
-                        {
-                            title: 'Heading 5',
-                            value: 'h5',
-                        },
-                        {
-                            title: 'Heading 6',
-                            value: 'h6',
-                        },
-                        {
-                            title: 'Paragraph',
-                            value: 'p',
-                        },
-                    ],
-                },
-            ],
+            type: 'myBlockContent',
         },
     ],
 
     preview: {
         select: {
             title: 'title',
-            body: 'body',
-            media: 'image',
+            // media: 'image',
         },
         //   prepare(selection) {
         //     const {author} = selection
--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
+// import _blockContent from './__blockContent';
 import blockContent from './blockContent';
 import category from './category';
 import post from './post';
