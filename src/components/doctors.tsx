38e64795541427b5ac2592961c1ba8444809f028
Colin Knebl <colinknebl@outlook.com>
2020-06-11 08:17:47 -0400

    add doctors component

---
+++ src/components/doctors.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { ContentView } from './content-view';
+import { getDoctors } from '../gql/queries/doctors';
+import type { Doctor } from '../models/Doctor';
+
+interface IDoctorsProps {
+    DoctorView?: (props: IDoctorViewProps) => JSX.Element;
+    column?: boolean;
+}
+
+export function Doctors(props: IDoctorsProps) {
+    const { doctors } = getDoctors();
+    const ListView = props.DoctorView || DoctorView;
+
+    return (
+        <StyledDoctorList column={props.column} className="doctors">
+            {doctors.map((doc, i) => (
+                <li key={i}>
+                    <ListView doctor={doc} />
+                </li>
+            ))}
+        </StyledDoctorList>
+    );
+}
+
+const StyledDoctorList = styled.ul<Pick<IDoctorsProps, 'column'>>`
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+
+    display: grid;
+    grid-template-columns: ${(p) =>
+        p.column ? '1fr' : 'repeat(auto-fill, minmax(320px, 1fr))'};
+    grid-gap: 50px;
+    gap: 50px;
+`;
+
+//#region DoctorView
+export interface IDoctorViewProps {
+    doctor: Doctor;
+}
+
+function DoctorView({ doctor }: IDoctorViewProps) {
+    return (
+        <StyledDoctorView>
+            <img
+                loading="lazy"
+                src={doctor.imageUrl}
+                alt={doctor.name}
+                height="350"
+                width="350"
+            />
+            <h3>{doctor.name}</h3>
+            <hr />
+            <ContentView content={doctor.content} />
+        </StyledDoctorView>
+    );
+}
+
+const StyledDoctorView = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        object-fit: contain;
+    }
+
+    hr {
+        border-top: 2px solid var(--primary-color);
+        width: 100%;
+        margin: 0;
+    }
+
+    p {
+        text-indent: 4ch;
+    }
+`;
+//#endregion DoctorView
--- src/pages/staff.tsx
+++ src/pages/staff.tsx
@@ -1,35 +1,31 @@
 import { graphql, PageProps } from 'gatsby';
 import { FluidObject } from 'gatsby-image';
-import styled from 'styled-components';
 import React from 'react';
 
 import Page from '../components/page';
 import SEO from '../components/seo';
 import { JumbotronSection } from '../components/jumbotron-section';
-import { Letter } from '../components/_temp/letter';
 import { Section } from '../components/section';
 import { Doctors, IDoctorViewProps } from '../components/doctors';
 
-const IndexPage = (props: PageProps<IPageQueryResults>) => {
+const TITLE = 'Staff';
+
+const StaffPage = (props: PageProps<IPageQueryResults>) => {
     return (
         <Page>
-            <SEO title="Home" />
+            <SEO title={TITLE} />
             <JumbotronSection
                 image={props.data.placeholderImage.childImageSharp.fluid}
-                title={props.data.site.siteMetadata.title}
-            >
-                <StyledDocNames>
-                    <Doctors DoctorView={DoctorName} column />
-                </StyledDocNames>
-            </JumbotronSection>
+                title={TITLE}
+            />
             <Section>
-                <Letter />
+                <Doctors />
             </Section>
         </Page>
     );
 };
 
-export default IndexPage;
+export default StaffPage;
 
 interface IPageQueryResults {
     placeholderImage: {
@@ -37,39 +33,18 @@ interface IPageQueryResults {
             fluid: FluidObject;
         };
     };
-    site: {
-        siteMetadata: {
-            title: string;
-        };
-    };
 }
 
 export const query = graphql`
     query {
-        placeholderImage: file(relativePath: { eq: "chair.jpg" }) {
+        placeholderImage: file(
+            relativePath: { eq: "office/treatment-room-4.jpg" }
+        ) {
             childImageSharp {
                 fluid(maxWidth: 800) {
                     ...GatsbyImageSharpFluid
                 }
             }
         }
-        site {
-            siteMetadata {
-                title
-            }
-        }
     }
 `;
-
-const StyledDocNames = styled.div`
-    text-align: center;
-
-    .doctors {
-        grid-gap: 0;
-        gap: 0;
-    }
-`;
-
-function DoctorName({ doctor }: IDoctorViewProps) {
-    return <li>{doctor.name}</li>;
-}
